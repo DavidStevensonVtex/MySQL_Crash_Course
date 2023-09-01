8fa78ac99c54469f0f0306cb5dd87f57984847ef
David Stevenson <drstevenson1958@yahoo.com>
2023-09-01 09:53:09 -0400

    Create table employee

---
+++ ch07_ComparingValues/create_table_employee.sql
@@ -0,0 +1,17 @@
+create table employee
+	(
+	emp_name		varchar(50),
+	retirement_date	date
+	);
+	
+insert into employee
+	(
+	emp_name,
+	retirement_date
+	)
+values
+	('Alfred','2034-01-08'),
+	('Latasha','2029-11-17'),
+	('Nancy', null),
+	('Chuck', null),
+	('Mitch', null);
