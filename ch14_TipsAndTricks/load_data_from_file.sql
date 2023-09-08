e993fbe0cdf2e994d13838a1a119137cf26f88ff
David Stevenson <drstevenson1958@yahoo.com>
2023-09-08 13:32:12 -0400

    Loading data from csv file with header row

--- ch14_TipsAndTricks/load_data_from_file.sql
+++ ch14_TipsAndTricks/load_data_from_file.sql
@@ -3,6 +3,7 @@ use investment ;
 load data local 
 infile 'D:/drs/MySQL/MySQL_Crash_Course/ch14_TipsAndTricks/market_indexes.csv' 
 into table market_index
-fields terminated by ',' ;
+fields terminated by ','
+ignore 1 lines ;
 
 select * from market_index;
