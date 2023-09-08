5d39a55e50c1bd1cf310045685626c7635cc1a77
David Stevenson <drstevenson1958@yahoo.com>
2023-09-08 13:29:47 -0400

    Loading data from a  comma separated file

--- ch14_TipsAndTricks/load_data_from_file.sql
+++ ch14_TipsAndTricks/load_data_from_file.sql
@@ -1,5 +1,8 @@
 use investment ;
 
-load data local infile 'D:/drs/MySQL/MySQL_Crash_Course/ch14_TipsAndTricks/market_indexes.txt' into table market_index;
+load data local 
+infile 'D:/drs/MySQL/MySQL_Crash_Course/ch14_TipsAndTricks/market_indexes.csv' 
+into table market_index
+fields terminated by ',' ;
 
 select * from market_index;
