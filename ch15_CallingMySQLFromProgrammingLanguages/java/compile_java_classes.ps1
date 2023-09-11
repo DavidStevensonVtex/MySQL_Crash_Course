26de6f06654a1f1c49f2a0a789f14eecb13693c8
David Stevenson <drstevenson1958@yahoo.com>
2023-09-11 12:51:58 -0400

    Java: Redirect output to MountainList.txt

--- ch15_CallingMySQLFromProgrammingLanguages/java/compile_java_classes.ps1
+++ ch15_CallingMySQLFromProgrammingLanguages/java/compile_java_classes.ps1
@@ -2,6 +2,6 @@ $env:path += "C:\Program Files\Java\jdk-20\bin:$env:path"
 $env:classpath = ".;C:\Program Files\Java\jdk-20\lib;C:\Program Files\MySQL\JavaLib\mysql-connector-j-8.1.0.jar"
 
 javac MountainList.java
-java MountainList
+java MountainList | Out-File MountainList.txt
 
 Remove-Item *.class
